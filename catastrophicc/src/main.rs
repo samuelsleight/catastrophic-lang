f7c72f0996c64c05e0920952731e5548d8cda551
samuelsleight <samuel.sleight@gmail.com>
2023-02-14 22:44:59 +0000

    ♻️ Refactor into a struct and some named methods
    This removes some capturing of the `Args` value, and hopefully slightly improves readability with some self-documenting method names

--- catastrophicc/src/main.rs
+++ catastrophicc/src/main.rs
@@ -30,39 +30,66 @@ struct Args {
     input: PathBuf,
 }
 
-fn debug_callback<Input: Debug>(debug: bool) -> impl FnOnce(&StageContext<Input>) -> Continue {
-    move |input| {
-        if debug {
-            println!("{:#?}", input.input);
-            Continue::Cancel
-        } else {
-            Continue::Continue
+struct App {
+    args: Args,
+}
+
+impl App {
+    fn new(args: Args) -> Self {
+        Self { args }
+    }
+
+    fn run(&self) -> Result<()> {
+        let pipeline_context = self.make_context()?;
+        let pipeline = self.make_pipeline();
+
+        let result = pipeline.run(pipeline_context);
+        self.finish(result)
+    }
+
+    fn make_context(&self) -> Result<StageContext<PathBuf>> {
+        let error_context = ErrorContext::from_file(&self.args.input)?;
+        let time_keeper = TimeKeeper::new("Overall");
+        let pipeline_context = StageContext::new(self.args.input.clone(), time_keeper, error_context);
+        Ok(pipeline_context)
+    }
+
+    fn make_pipeline(&self) -> impl RunPipeline<anyhow::Error, Start = StageContext<PathBuf>, End = StageContext<()>> {
+        pipeline(ParseStage.stage(), self.debug_callback(DebugMode::Ast))
+            .and_then(AnalysisStage.stage(), self.debug_callback(DebugMode::Hir))
+            .and_then(OptimizationStage.stage(), self.debug_callback(DebugMode::Mir))
+            .and_then(CompilationStage.stage(), |_| ())
+    }
+
+    fn debug_callback<Input: Debug>(&self, debug: DebugMode) -> impl FnOnce(&StageContext<Input>) -> Continue {
+        let debug = self.args.debug == Some(debug);
+
+        move |input| {
+            if debug {
+                println!("{:#?}", input.input);
+                Continue::Cancel
+            } else {
+                Continue::Continue
+            }
+        }
+    }
+
+    fn finish(&self, result: Result<StageContext<()>, PipelineError<anyhow::Error>>) -> Result<()> {
+        match result {
+            Ok(context) => {
+                if self.args.profile {
+                    context.time_keeper.finish()
+                }
+
+                Ok(())
+            }
+            Err(PipelineError::Cancelled) => Ok(()),
+            Err(PipelineError::Err(error)) => Err(error),
         }
     }
 }
 
 fn main() -> Result<()> {
     let args = Args::try_parse()?;
-
-    let error_context = ErrorContext::from_file(&args.input)?;
-    let time_keeper = TimeKeeper::new("Overall");
-    let pipeline_context = StageContext::new(args.input, time_keeper, error_context);
-
-    let result = pipeline(ParseStage.stage(), debug_callback(args.debug == Some(DebugMode::Ast)))
-        .and_then(AnalysisStage.stage(), debug_callback(args.debug == Some(DebugMode::Hir)))
-        .and_then(OptimizationStage.stage(), debug_callback(args.debug == Some(DebugMode::Mir)))
-        .and_then(CompilationStage.stage(), |_| ())
-        .run(pipeline_context);
-
-    match result {
-        Ok(context) => {
-            if args.profile {
-                context.time_keeper.finish()
-            }
-
-            Ok(())
-        }
-        Err(PipelineError::Cancelled) => Ok(()),
-        Err(PipelineError::Err(error)) => Err(error),
-    }
+    App::new(args).run()
 }
